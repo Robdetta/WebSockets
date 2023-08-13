b384bb6d3fa92bda7181459e0b0546d13ecb968c
robbie <robdetta@gmail.com>
2023-08-13 14:49:10 -0400

    transfering

--- socketIO101/basics.js
+++ socketIO101/basics.js
@@ -1,6 +1,18 @@
-import express from 'express';
+/* eslint-disable @typescript-eslint/no-var-requires */
+const express = require('express');
 const app = express();
-import socketio from 'socket.io';
+const socketio = require('socket.io');
+
+app.use(express.static(__dirname + '/public'));
 
 const expressServer = app.listen(8000);
 const io = socketio(expressServer);
+
+io.on('connection', (socket) => {
+  console.log(socket.id, 'has connected');
+  //in ws we use send method and in socketio we use the emit method
+  socket.emit('messageFromServer', { data: 'Welcome to the socket server!' });
+  socket.on('messageFromClient', (data) => {
+    console.log('Data:', data);
+  });
+});
